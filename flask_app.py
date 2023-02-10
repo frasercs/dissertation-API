2e162c002d580de257c8bb4b181a5f4da0d54129
Fraser Macdonald CSM2019 <fraser.macdonald.2019@uni.strath.ac.uk>
2023-02-10 07:55:43 +0000

    Implemented 2 new endpoints to get the data and the matrix

--- flask_app.py
+++ flask_app.py
@@ -6,18 +6,20 @@ from flask_restx import Api, Resource, fields
 from openpyxl import load_workbook
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Diagnosis API', description='A simple API to diagnose animals')
+api = Api(app, version='1.0', title='Diagnosis API', description='A simple API to diagnose animals using Bayes Theorem', default= 'Diagnosis API', default_label='Diagnosis API')
 
-model = api.model('Diagnose', {
+
+diagnosis_model = api.model('Diagnose', {
     'animal': fields.String(required=True, description='The type of animal'),
     'symptoms': fields.List(fields.String, required=True, description='The symptoms shown by the animal')
 })
 
 
-@api.route('/diagnose/', methods=['POST'])
+@api.route('/api/diagnose/', methods=['POST'])
+
 @api.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
          params={'animal': 'The type of animal to be diagnosed', 'symptoms': 'The symptoms shown by the animal'})
-@api.expect(model)
+@api.expect(diagnosis_model)
 class diagnose(Resource):
     def post(self):
         # Get the data from the API request
@@ -34,7 +36,10 @@ class diagnose(Resource):
         results = {}
 
         # Get the correct data from the data Excel sheet
-        diseases, likelihoods = get_disease_data(animal)
+        likelihoods = get_likelihood_data(animal)
+
+        # Get the list of diseases
+        diseases = get_diseases(animal)
 
         # TODO: alter this down the line to take data from an API request so we don't always
         #  have to assume priors are equal
@@ -63,37 +68,63 @@ class diagnose(Resource):
 
         return jsonify({'results': normalised_results})
 
+@api.route('/api/data/<string:animal>')
+
+@api.doc(responses={200: 'OK', 400: 'Invalid Argument'})
+class diagnosis_data(Resource):
+    def get(self, animal):
+        # Load the correct Excel sheet
+        ws = load_spreadsheet(animal)
+
+        # Get the correct data from the data Excel sheet
+        likelihoods = get_likelihood_data(animal)
+
+        # Get the list of diseases
+        diseases = get_diseases(animal)
+
+        # Get the list of symptoms
+        symptoms = get_symptoms(animal)
+
+        return jsonify({'possible diseases': diseases, 'required symptoms': symptoms, 'likelihoods': likelihoods})
+
+@api.route('/api/matrix/<string:animal>')
+
+@api.doc(responses={200: 'OK', 400: 'Invalid Argument'})
+class disease_symptom_matrix(Resource):
+    def get(self, animal):
+        # Load the correct Excel sheet
+        data = get_likelihood_data(animal)
+        return jsonify(data)
+
 
 # A function used to collect the data from the Excel workbook which I manually formatted to ensure the data works.
-def get_disease_data(animal):
-    # Get the workbook (the file path is currently set to the path used on pythonanywhere but this should be changed as
-    # needed).
-    wb = load_workbook(filename=os.path.join(sys.path[0], "data.xlsx"))
-    # Animal is the string passed in the function which directs to the correct sheet in the workbook
-    ws = wb[animal]
-    # List which stores every given disease
-    diseases = []
-    # Populate the list of diseases
-    for row in ws.iter_rows(min_row=2, max_col=1, max_row=ws.max_row):
-        for cell in row:
-            diseases.append(cell.value)
-    # List which stores every given symptom
-    symptoms = []
-    # Populate the list of symptoms
-    for col in ws.iter_cols(min_col=2, max_row=1):
-        for cell in col:
-            symptoms.append(cell.value)
+def get_likelihood_data(animal):
+    # Load the correct Excel sheet
+    ws = load_spreadsheet(animal)
+
+    # Get the list of symptoms and diseases
+    symptoms = get_symptoms(animal)
+    diseases = get_diseases(animal)
+
     # Dictionary which stores the likelihoods of each disease
     likelihoods = {}
-    # Loop through all rows in the workbook
+
+    # Counter used to keep track of the current disease
     disease_counter = 0
+
+    # Loop through all rows in the workbook
     for row in ws.rows:
         # Skip the first row as it is just the headers
         if row[0].value == 'Disease':
             disease_counter = 0
             continue
+
+        # Counter used to keep track of the current symptom
         symptom_counter = 0
+        # Dictionary which stores the likelihoods of each symptom for the current disease
         current_disease_likelihoods = {}
+
+        # Loop through all cells in each row except the first one as it is the disease name
         for cell in row[1:]:
             chance = cell.value
             current_disease_likelihoods[symptoms[symptom_counter]] = chance
@@ -101,7 +132,34 @@ def get_disease_data(animal):
         likelihoods[diseases[disease_counter]] = current_disease_likelihoods
         disease_counter += 1
 
-    return diseases, likelihoods
+    return likelihoods
+
+
+def get_diseases(animal):
+    # List which stores every given disease
+    diseases = []
+    ws = load_spreadsheet(animal)
+    # Populate the list of diseases
+    for row in ws.iter_rows(min_row=2, max_col=1, max_row=ws.max_row):
+        for cell in row:
+            diseases.append(cell.value)
+    return diseases
+
+
+def get_symptoms(animal):
+    # List which stores every given symptom
+    symptoms = []
+    ws = load_spreadsheet(animal)
+    # Populate the list of symptoms
+    for col in ws.iter_cols(min_col=2, max_row=1):
+        for cell in col:
+            symptoms.append(cell.value)
+    return symptoms
+
+
+def load_spreadsheet(animal):
+    workbook = load_workbook(filename=os.path.join(sys.path[0], "data.xlsx"))
+    return workbook[animal]
 
 
 # A function used to normalise the outputs of the bayes calculation
